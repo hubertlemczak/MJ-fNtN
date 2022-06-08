5c61acafbcfa4ac002957f715b0d94dffd510549
Hubert Lemczak <hubertlemczak@gmail.com>
2022-06-08 23:03:53 +0100

    7

---
+++ 7/index.js
@@ -0,0 +1,21 @@
+// const pattern = /^[a-z]{6,}$/;
+// console.log(pattern.test('shaunpshaunp'));
+
+// const products = [
+//   {
+//     name: '123',
+//     price: 33,
+//   },
+//   {
+//     name: '1234',
+//     price: 13,
+//   },
+// ];
+
+// const products2 = products.map((product) => {
+//   if (product.price > 30) product = { name: '123', price: 33 / 2 };
+//   return product;
+// });
+
+// console.log(products);
+// console.log(products2);
